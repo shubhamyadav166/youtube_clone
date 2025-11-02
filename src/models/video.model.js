dcbbe53935988b93a973e33e85b0cc9f2b9ce246
Shubham yadav <shubhamyadav1662000@gmail.com>
2025-11-02 15:16:37 +0530

    multer add for upload video image on cloudinary

---
+++ src/models/video.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, //////Cloudinary url
+        required: true,
+    },
+    thumbnail: {
+        type: String, /// Cloudinary url
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    discription: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+export const Video = mongoose.model("video", videoSchema);
